b7e413136345908122b92f84eaf1d298a2434fcd
hadr-im <hadrumet.im.department@gmail.com>
2025-08-27 21:24:22 +0100

    Fixin the PWA

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "@/styles/globals.css";
+import { registerSW } from "virtual:pwa-register";
+
+registerSW(); // that’s it
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,40 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['vite.svg'],
+      manifest: {
+        name: 'Hadrumet Passport',
+        short_name: 'Passport',
+        description: 'Your digital passport for Hadrumet',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'images/icons/splash-128.png',
+            sizes: '128x128',
+            type: 'image/png',
+          },
+          {
+            src: 'images/icons/splash-256.png',
+            sizes: '256x256',
+            type: 'image/png',
+          },
+          {
+            src: 'images/icons/splash-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": resolve(__dirname, "./src"),
